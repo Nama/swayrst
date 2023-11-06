4bdc41147e0b3d0dd353789380292df33700c3e9
Yama <mrt690@gmail.com>
2023-11-06 23:13:10 +0100

    add requirements in setup.py [no actions]

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(
     author='Yama',
     author_email='',
     description='Restore workspaces in sway to displays and move applications to saved workspaces.',
+    install_requires=[
+        'sh',
+        'git+https://github.com/Nama/swayrst.git'
+    ],
     entry_points={
         'console_scripts': [
             'swayrst = swayrst:main',
